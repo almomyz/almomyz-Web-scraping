23dd8ae7d478eb99aedd46608ecd0b2ae066db9a
Hesham Mohammed Noaman <140953153+HeshamNoaman@users.noreply.github.com>
2024-05-04 20:04:16 +0300

    fix fetching the pages links

--- index.ts
+++ index.ts
@@ -1,12 +1,9 @@
-// main.js
-import { getQuotes } from './abdulwahed'; // Adjust the path according to your file structure
-import puppeteer from 'puppeteer';
-async function main() {
-    
-    try {
-   let links= await getQuotes();
-        console.log('Quotes fetched successfully.');
+import { getQuotes } from './abdulwahed';
 
+async function main() {
+    try {
+        const links = await getQuotes();
+        console.log('Quotes fetched successfully.');
         console.log('Results:', links);
     } catch (error) {
         console.error('Failed to fetch quotes:', error);
